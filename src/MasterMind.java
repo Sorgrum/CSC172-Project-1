56a51e0beab4ead52c6b84a307471f14bcf50e1f
Sorgrum <marcelogheiler@gmail.com>
2016-01-29 16:52:18 -0500

    Starting to segment everything into its own method

---
+++ src/MasterMind.java
@@ -0,0 +1,14 @@
+/**
+ * Student Name: Marcelo Gheiler
+ * Filename: MasterMind
+ * Date: 1/29/16
+ * TA Name: Colin Pronovost
+ * Assignment:
+ * Lab Day: Monday
+ * Lab Time: 5PM
+ * Lab Location: CSB 703
+ * I affirm that I have not given or received any unauthorized help on this assignment, and that this work is my own
+ */
+public class MasterMind {
+
+}
